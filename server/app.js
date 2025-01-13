c1c93cacf9c47cef9c382936c0f4a37133c3e173
apol <arnoldpollycarp@gmail.com>
2025-01-13 18:10:18 +0300

    Reg Functionality

--- server/app.js
+++ server/app.js
@@ -11,7 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(
+ { origin: 'http://localhost:5173', // Specify the allowed origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true,
+}));
 
 /**
  * Routes
